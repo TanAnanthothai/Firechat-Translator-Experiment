1d1368017c0f7ae5049c38d8af70c844c8612f7c
Tan Ananthothai <teerakorn.a@gmail.com>
2018-06-03 01:45:52 +0700

    import premade css/jss into vue component and main.js
    refer to here https://bootsnipp.com/snippets/0e3Ma

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+require('./assets/css/chat.css')
+
+// import 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,300'
+// require('//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css')
+// require('//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js')
+// require('//code.jquery.com/jquery-1.11.1.min.js')
 
 new Vue({
   el: '#app',
